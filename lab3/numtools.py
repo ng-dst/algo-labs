f6f661652a9b34bced7a9422693a7ee37a25b839
Your Name <you@example.com>
2022-04-01 17:57:34 -0400

    lab 3 completed

---
+++ lab3/numtools.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import random
+from math import gcd
+
+
+def witness(a, n):
+    if n == 2 or n == 3:
+        return False
+    if n % 2 == 0:
+        return True
+    u = n - 1
+    t = 0
+    while u & 1 == 0:
+        t += 1
+        u >>= 1
+    x = pow(a, u, n)
+    for i in range(t):
+        y = pow(x, 2, n)
+        if y == 1 and x != 1 and x != n-1:
+            return True
+        x = y
+    return x != 1
+
+
+def miller_rabin(n, s=20):
+    for _ in range(s):
+        a = random.randint(1, n-1)
+        if witness(a, n):
+            return False  # composite
+    return True  # probably prime
+
+
+def pollard_rho(n):
+    if n <= 3:
+        return n
+    if n % 2 == 0:
+        return 2
+    i = 1
+    x = random.randint(0, n-1)
+    y = x
+    k = 2
+    cycle = set()
+    while True:
+        i += 1
+        if x in cycle:
+            return n
+        else:
+            cycle.add(x)
+        x = (x**2 - 1) % n
+        d = gcd(y - x, n)
+        if d != 1 and d != n:
+            return d
+        if i == k:
+            y = x
+            k *= 2
+
+
+def factorize(n):
+    factors = dict()
+    while n > 1:
+        if not miller_rabin(n):
+            d = pollard_rho(n)
+            n //= d
+            if not miller_rabin(d):
+                fd = factorize(d)
+                factors = sum_dicts(factors, fd)
+            else:
+                factors = sum_dicts(factors, {d: 1})
+        else:
+            factors = sum_dicts(factors, {n: 1})
+            break
+    return factors
+
+
+def sum_dicts(d1, d2):
+    for k, v in d2.items():
+        if k not in d1.keys():
+            d1[k] = v
+        else:
+            d1[k] += v
+    return d1
